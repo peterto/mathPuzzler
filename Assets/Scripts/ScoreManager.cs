df4d5f35b43f07714f3793d0dffc03626504c022
Peter To <pt9386@gmail.com>
2016-07-30 20:00:44 -0400

    Initial commit

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    [SerializeField]
+    private Text scoreText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
